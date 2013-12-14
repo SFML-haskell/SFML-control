a6ca79a863b067f67efed918531e293648fefd39
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2013-12-14 09:33:34 +0000

    Improved docs

--- src/Control/Monad/SFML.hs
+++ src/Control/Monad/SFML.hs
@@ -1,9 +1,64 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
+
+{-|
+
+This package builds on top of @SFML@ and offers an higher level of
+abstraction over the original library, mainly exposing the 'SFML' monad,
+which is responsible for allocating and running the destructors for your
+managed objects. This allow us to program at an higher level of abstraction:
+
+
+> import Control.Monad.SFML
+> import qualified SFML.Graphics as G
+> import qualified SFML.Window as W
+> import SFML.Graphics.Color
+> 
+> import Paths_SFMLExamples
+> 
+> main :: IO ()
+> main = runSFML $ do
+>     let ctxSettings = Just $ W.ContextSettings 24 8 0 1 2
+>     wnd <- createRenderWindow (W.VideoMode 640 480 32)
+>            "SFML-Control Demo" [W.SFDefaultStyle] ctxSettings
+>     logoPath  <- liftIO $ getDataFileName "Haskell-Logo.png"
+>     fontPath  <- liftIO $ getDataFileName "Vera.ttf"
+>     musicPath <- liftIO $ getDataFileName "DST-BreakOut.ogg"
+>     tex <- textureFromFile logoPath Nothing
+>     spr <- createSprite
+>     fnt <- fontFromFile fontPath
+>     txt <- createText
+>     setTextString txt "Haskell-Control\nhandles memory\nfor you"
+>     setTextFont txt fnt
+>     setTextCharacterSize txt 20
+>     setTextColor txt blue
+>     msc <- musicFromFile musicPath
+>     play msc
+>     setTexture spr tex True
+>     loop wnd spr txt
+> 
+> 
+> loop :: G.RenderWindow -> G.Sprite -> G.Text -> SFML ()
+> loop wnd spr txt = do
+>     drawSprite wnd spr Nothing
+>     drawText   wnd txt $ Just (G.renderStates { G.transform = G.translation 460 40 })
+>     display wnd
+>     evt <- waitEvent wnd
+>     case evt of
+>         Nothing -> return ()
+>         Just W.SFEvtClosed -> return ()
+>         _ -> loop wnd spr txt
+
+-}
+
+
 module Control.Monad.SFML
   ( module Control.Monad.SFML.Types
+  -- * Conversions
+  -- $THConversion
   , module Control.Monad.SFML.Conversions
+  -- * Re-exports for your convenience
   , liftIO
   ) where
 
@@ -18,6 +73,11 @@ import Control.Monad.SFML.Types.Internal
 import Control.Monad.SFML.Types
 import Control.Monad.SFML.Conversions
 
+{- $THConversion
+
+Almost the totality of this package is generated via Template Haskell.
+
+-}
 
 --------------------------------------------------------------------------------
 drawRectangleOfSize :: Vec2f -> SFML G.RectangleShape
--- src/Control/Monad/SFML/Types.hs
+++ src/Control/Monad/SFML/Types.hs
@@ -8,5 +8,7 @@ import Control.Monad.SFML.Types.Internal
 
 --------------------------------------------------------------------------------
 -- | Run the SFML monad, calling all the destructors appropriately.
+-- The library is designed to force you to call this function to get back into
+-- the 'IO' monad, so that every object is appropriately destroyed.
 runSFML :: SFML a -> IO ()
 runSFML (SFML m) = join . fmap sequence_ . flip execStateT [] $ m
